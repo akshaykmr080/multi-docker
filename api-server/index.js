119b7d671ceb145c7e96e86b160c49a19744235e
ashreekanth <ashreekanth@vmware.com>
2020-06-21 14:20:57 -0700

    test pg connection - 2

--- api-server/index.js
+++ api-server/index.js
@@ -14,14 +14,15 @@ const { Pool } = require('pg');
 console.log(keys.pgUser);
 console.log(keys.pgUser);
 console.log(keys.pgPassword);
+console.log(keys.pgHost);
 console.log(keys.pgPort);
 const pgClient = new Pool({
-    user: keys.pgUser,
-    host: keys.pgDatabase,
-    database: keys.pgDatabase,
-    password: keys.pgPassword,
-    port: keys.pgPort
-})
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
+});
 
 pgClient.on('connect', () => {
     pgClient
